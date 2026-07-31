9125e2a3ff1832dd59831f9050797cf4f9a01dcd
Norman Fellows <norman.fellows@orange.fr>
2026-07-31 12:41:18 +0200

    Update viewer.js
    Deleted everything in the editor and pasted this in as a complete replacement

--- js/viewer.js
+++ js/viewer.js
@@ -3,16 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Scrapbook Viewer Version 0.3");
 
     Mirador.viewer({
-
         id: "viewer-stage",
-
         windows: [
             {
-                manifestId:
-                "https://normanfellows.github.io/scrapbook-viewer/manifests/manifest-00.json"
+                manifestId: "https://normanfellows.github.io/scrapbook-viewer/manifests/manifest-00.json"
             }
         ]
-
     });
 
 });
