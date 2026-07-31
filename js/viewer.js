98089c139924fe714d7051a711e43eb554bb40d5
Norman Fellows <norman.fellows@orange.fr>
2026-07-31 09:52:37 +0200

    Update viewer.js
    Replaced the contents with document.addEventListener("DOMContentLoaded", () => {
    
        console.log("Scrapbook Viewer Version 0.2");
    
        Mirador.viewer({
            id: "viewer-stage"
        });
    
    });

--- js/viewer.js
+++ js/viewer.js
@@ -1,10 +1,9 @@
-/* ===========================================
-   SCRAPBOOK VIEWER
-   Version 0.1
-   =========================================== */
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Scrapbook Viewer Version 0.1");
+    console.log("Scrapbook Viewer Version 0.2");
+
+    Mirador.viewer({
+        id: "viewer-stage"
+    });
 
 });
