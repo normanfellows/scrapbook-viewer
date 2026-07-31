b095e7972df8221f5bf16b03f6ba49099046fda0
Norman Fellows <norman.fellows@orange.fr>
2026-07-31 11:56:29 +0200

    Update viewer.js
    Replaced with:
    
    document.addEventListener("DOMContentLoaded", () => {
    
        console.log("Scrapbook Viewer Version 0.3");
    
        Mirador.viewer({
    
            id: "viewer-stage",
    
            windows: [
                {
                    manifestId:
                    "https://normanfellows.github.io/scrapbook-viewer/manifests/manifest-00.json"
                }
            ]
    
        });
    
    });

--- js/viewer.js
+++ js/viewer.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Scrapbook Viewer Version 0.2");
+    console.log("Scrapbook Viewer Version 0.3");
 
     Mirador.viewer({
-    id: "viewer-stage",
-    windows: [
-        {
-            manifestId: "https://normanfellows.github.io/scrapbook-viewer/manifests/manifest-00.json"
-        }
-    ]
-});;
+
+        id: "viewer-stage",
+
+        windows: [
+            {
+                manifestId:
+                "https://normanfellows.github.io/scrapbook-viewer/manifests/manifest-00.json"
+            }
+        ]
+
+    });
+
+});
